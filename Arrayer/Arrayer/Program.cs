1d1a3005543907807b4403fa27f1be079c3feb6c
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-11-18 12:17:05 +0100

    Medelvärde med array

---
+++ Arrayer/Arrayer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temp = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+            Console.WriteLine("Medelvärde: " + Medel(temp));
+        }
+        public static double Medel(double[] a)
+        {
+            double summa = 0;
+			foreach (double v1 in a)
+				summa += v1;
+            return (summa / a.Length);
+        }
+    }
+}
